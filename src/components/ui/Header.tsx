6bbc78ba04bb35383e268e2a7a9df9dfe9d5b1c1
Maichiee <imam.harits24@gmail.com>
2024-12-12 22:27:56 +0700

    first commit

--- src/components/ui/Header.tsx
+++ src/components/ui/Header.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 import { useState, useEffect } from 'react'
-import { motion, useAnimationControls } from 'framer-motion'
+import { motion } from 'framer-motion'
 import { useInView } from 'react-intersection-observer'
 import { Sparkles } from 'lucide-react'
 import heroImage from "./../../assets/images/hero2.jpg"
